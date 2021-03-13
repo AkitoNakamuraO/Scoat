30464cfc90eb7db5de6b0a83515e002540290764
Akito Nakamura <akito.nakamura.c@gmail.com>
2021-03-13 15:12:51 +0900

    DB接続に使う関数を作りました

---
+++ public/parts/connectDB.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+  host: "us-cdbr-east-03.cleardb.com",
+  port: 3306,
+  user: "b2d3e150901925",
+  password: "c6661e08",
+  database: "b2d3e150901925",
+});
+
+const createConnection = function () {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((error, connection) => {
+      if (error) reject(error);
+      resolve(connection);
+    });
+  });
+};
+
+module.exports = createConnection;
