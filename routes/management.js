8d111e3ec9aa1a9bca09478bc575230d3b2b86ca
Takuya <uoastudent182@gmail.com>
2021-03-14 18:20:11 +0900

    first commit

--- routes/management.js
+++ routes/management.js
@@ -1,4 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+router.get("/", (req, res, next) => {
+  res.render("management", {title: "Scoat"});
+});
+
 module.exports = router;
