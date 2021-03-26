e26b1fc3f3defdce0b15fa35419ef9af6d9ba868
AkitoNakamura <akito.nakamura.c@gmail.com>
2021-03-26 19:44:39 +0900

    jsを少し編集

---
+++ public/javascripts/schedule.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// 予定を取得
+const getSchedule = function () {
+  const request = new XMLHttpRequest();
+  const requestURL = "/schedule/get-schedules/" + spaceId;
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+  request.onload = function () {
+    const schedules = request.response;
+    printSchedules(schedules);
+  };
+};
+
+// 一週間分の列を作る
+const createWeek = function (today) {
+  const schedulesArea = document.getElementById("schedules-area");
+  const WEEK = 7;
+  const TIME = 24;
+  for (let i = 0; i <= WEEK; i++) {
+    const day = document.createElement("div");
+    const date = document.createElement("div");
+    date.innerHTML = today.getMonth() + 1 + "/" + today.getDate();
+    const time = document.createElement("div");
+    for (let i = 0; i < TIME * 6; i++) {
+      const cell = document.createElement("div");
+      cell.innerHTML = "a";
+      time.appendChild(cell);
+    }
+    // dayに日付要素を追加
+    day.appendChild(date);
+    // dayに時間要素を追加する
+    day.appendChild(time);
+    // 一日分をスケジュールエリアに追加する
+    schedulesArea.appendChild(day);
+    // 次の日に設定する
+    today.setDate(today.getDate() + 1);
+  }
+};
+
+createWeek(new Date());
--- routes/schedule.js
+++ routes/schedule.js
@@ -2,4 +2,15 @@ var express = require("express");
 var router = express.Router();
 const createConnectio = require("../parts/connectDB");
 
+const getSchedule = function (spaceId) {};
+
+// 画面を表示
+router.get("/", function (req, res, next) {
+  res.render("schedule");
+});
+
+router.get("/get-schedules/:space-id", function (req, res, next) {
+  res.json();
+});
+
 module.exports = router;
