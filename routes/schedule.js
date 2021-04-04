5c91d663b39de3944ea84c0690bb6121090372fc
Takuya <uoastudent182@gmail.com>
2021-04-04 17:23:17 +0900

    オブジェクトつくる

--- routes/schedule.js
+++ routes/schedule.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const createConnection = require("../parts/connectDB");
 const moment = require("moment");
 
-function dateFormat(date, format){
+function dateFormat(date, format) {
   format = format.replace(/YYYY/, date.getFullYear());
   format = format.replace(/MM/, date.getMonth() + 1);
   format = format.replace(/DD/, date.getDate());
 
-    return format;
+  return format;
 }
 
 const getSchedules = function (sql, spaceId) {
@@ -46,15 +46,47 @@ router.get("/space/:id", function (req, res, next) {
 // スケジュール情報を取得
 router.get("/get-schedules", async function (req, res, next) {
   // 拓也が作るところ
-  var today = moment().format("YYYY-MM-DD");
-  var weekend = today.add(7,"days").format("YYYY-MM-DD");
+  var reschedules = [];
+  var today = moment();
+  var weekend = today.clone().add(7, "days");
+  var between = [];
   const spaceId = req.session.spaceId;
-  const sql = `SELECT * FROM schedules WHERE space_id = ? AND schedule_date BETWEEN ${today} AND ${weekend}`;
+  const sql = `SELECT * FROM schedules WHERE space_id = ?`;
   const schedules = await getSchedules(sql, spaceId);
-  for(var ){
-    
+
+  console.log(today);
+  console.log(weekend);
+  var j = 0;
+  var i = 0;
+  for (i = 0; i < schedules.length; i++) {
+    if (today.isSameOrBefore(schedules[i].schedule_date) && weekend.isSameOrAfter(schedules[i].schedule_date)) {
+      between[j] = schedules[i];
+      j++;
+    }
   }
-  res.json(schedules);
+
+  console.log(between);
+  console.log("\\\\\\\\\\");
+  console.log(schedules);
+  for(i=0;i<7;i++){
+    reschedules[i] = [];
+    today.add(1,"days");
+    for(j=0;;j++){
+      if(between[j] == null) break;
+      reschedules[i][j]={};
+      if(today.isSame(between[j].schedule_date, "days")){
+        reschedules[i][j].id = between[j].schedule_id;
+        reschedules[i][j].date = moment(between[j].schedule_date).format("YYYY/MM/DD");
+        reschedules[i][j].startHour = between[j].schedule_start_time.substr(0, 2);
+        reschedules[i][j].endHour = between[j].schedule_end_time.substr(0, 2);
+        reschedules[i][j].startMinute = between[j].schedule_start_time.substr(3, 2);
+        reschedules[i][j].endMinute = between[j].schedule_end_time.substr(3,2);
+        reschedules[i][j].content = between[j].schedule_contents;
+      }
+    }
+  }
+  console.log(reschedules);
+  res.json(reschedules);
 });
 
 // 予定登録
