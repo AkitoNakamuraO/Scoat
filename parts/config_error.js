77bea9644bccfad033be7016728aaa327835cdaa
AkitoNakamura <akito.nakamura.c@gmail.com>
2021-05-23 01:27:58 +0900

    登録処理の際にログインに精するミスを修正。

--- parts/config_error.js
+++ parts/config_error.js
@@ -23,12 +23,8 @@ function isEmpty(req, res, next) {
   if (mail === "") mailErrors.push("Eメールを入力してください。");
   if (password === "") passErrors.push("パスワードを入力してください。");
 
-  if (
-    locationErrors.length > 0 ||
-    mailErrors.length > 0 ||
-    passErrors.length > 0
-  ) {
-    res.render("login", {
+  if (locationErrors.length > 0 || mailErrors.length > 0 || passErrors.length > 0) {
+    res.render("register", {
       locationErrors: locationErrors,
       mailErrors: mailErrors,
       passErrors: passErrors,
@@ -38,29 +34,29 @@ function isEmpty(req, res, next) {
 
 function isCorrect(req, res, next) {
   const mailErrors = [];
-  const reg = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/; 
+  const reg = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
   const email = req.body.mail;
 
   if (reg.test(email) === false) mailErrors.push("有効なメールアドレスを入力してください。");
 
-  if (mailErrors.length > 0) res.render("login", {locationErrors: [], mailErrors: mailErrors, passErrors: []});
+  if (mailErrors.length > 0) res.render("register", { locationErrors: [], mailErrors: mailErrors, passErrors: [] });
   else next();
 }
 
-async function isUnique(req, res, next){
+async function isUnique(req, res, next) {
   let file = "register";
   const location = req.body.location;
   const mail = req.body.mail;
   let mailErrors = [];
-  let sql = "SELECT * FROM admins JOIN spaces ON admins.space_id = spaces.space_id WHERE admins.admin_email ='"+mail+"';";
-  if(req.session.spaceId != undefined){
-    sql = "SELECT * FROM admins JOIN spaces ON admins.space_id = spaces.space_id WHERE admins.admin_email ='"+req.session.mail+"';";
+  let sql = "SELECT * FROM admins JOIN spaces ON admins.space_id = spaces.space_id WHERE admins.admin_email ='" + mail + "';";
+  if (req.session.spaceId != undefined) {
+    sql = "SELECT * FROM admins JOIN spaces ON admins.space_id = spaces.space_id WHERE admins.admin_email ='" + req.session.mail + "';";
     file = "updatePlace";
   }
   const data = await checkData(sql);
 
-  for(let i=0;i<data.length;i++){
-    if(location == data[i].space_name){
+  for (let i = 0; i < data.length; i++) {
+    if (location == data[i].space_name) {
       mailErrors.push("場所が重複しています。");
       mailErrors.push("(※)1つのメールに複数の同じ名前の場所は登録できません。");
       mailErrors.join("\n");
@@ -68,12 +64,12 @@ async function isUnique(req, res, next){
     }
   }
 
-  if(file=="updatePlace" && req.body.location == req.session.location) mailErrors=[];
-  if(mailErrors.length > 0) res.render(file, {locationErrors: [], mailErrors: mailErrors, passErrors: []});
+  if (file == "updatePlace" && req.body.location == req.session.location) mailErrors = [];
+  if (mailErrors.length > 0) res.render(file, { locationErrors: [], mailErrors: mailErrors, passErrors: [] });
   else next();
 }
 
-function checkPassword(req, res, next){
+function checkPassword(req, res, next) {
   // 入力値取得
   const password = req.body.password;
   const confirm = req.body.confirm;
@@ -91,4 +87,4 @@ function checkPassword(req, res, next){
   }
 }
 
-module.exports = { isEmpty, isCorrect, isUnique, checkPassword};
+module.exports = { isEmpty, isCorrect, isUnique, checkPassword };
